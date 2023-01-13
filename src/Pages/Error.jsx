a7341ece31e00572dfae34a5de5dc48ec69f0a34
mydax <41824317+mydax-kid@users.noreply.github.com>
2023-01-13 12:08:26 +0000

    added error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 
 const Error = () => {
   return(
-    <div className="section">
-      <h2>Oops, so much empty ☻</h2>
-      <Link to='/' className= 'link'>Go Back</Link>
+    <div className="section-error">
+      <h2>Oops, no such page exists :(</h2>
+      <Link to='/' className= 'btn'>Go Back</Link>
     </div>
   )
 }
